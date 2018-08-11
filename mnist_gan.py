e1577752bcfd6bbbb49dd20b2fa22a3225bd5f3d
Apratim Mukherjee <apratim101@gmail.com>
2018-08-11 19:50:25 +0530

    Added training to gan

--- mnist_gan.py
+++ mnist_gan.py
@@ -19,6 +19,16 @@ test_data = datasets.MNIST(root="./data/", train=False, transform=compose, downl
 train_load = DataLoader(dataset=train_data, batch_size=32, shuffle=True)
 test_load = DataLoader(dataset=test_data, batch_size=32, shuffle=False)
 
+def display(loader):
+	stack = (loader[0].view(1, 28, 28)).data.numpy()
+	stack = np.squeeze(stack)
+	for i in range(8):
+		img = (loader[i+1].view(1, 28, 28)).data.numpy()
+		img = np.squeeze(img)
+		stack = np.hstack((img, stack))
+		cv2.imshow("Test", stack)
+		cv2.waitKey(1)
+
 class Discriminator(nn.Module):
 	def __init__(self):
 		super(Discriminator, self).__init__()
@@ -85,7 +95,7 @@ class Generator(nn.Module):
 disc = Discriminator()
 gen = Generator()
 
-criterion = nn.CrossEntropyLoss()
+criterion = nn.BCELoss()
 optimizer_gen = optim.Adam(gen.parameters(), lr = 0.001)
 optimizer_disc = optim.SGD(disc.parameters(), lr = 0.001, momentum = 0.5)
 
@@ -94,3 +104,43 @@ print "Discriminator parameters : {}".format(total_params)
 
 total_params = sum(p.numel() for p in gen.parameters() if p.requires_grad)
 print "Generator parameters : {}".format(total_params)
+
+def train_gan(epochs):
+	disc.train()
+	gen.train()
+	for epoch in range(epochs):
+		for i, data in enumerate(train_load):
+			
+			inputs, _ = data
+			labels = Variable(torch.ones(inputs.size(0)))
+
+			outputs = disc(Variable(inputs))
+
+			real_loss = criterion(outputs, labels)
+
+			noise = Variable(torch.randn(inputs.size(0), 100))
+			fake_inputs = gen(noise)
+
+			fake_labels = Variable(torch.zeros(inputs.size(0)))
+
+			outputs = disc(fake_inputs)
+
+			fake_loss = criterion(outputs, fake_labels)
+
+			loss = real_loss + fake_loss
+
+			gen_loss = criterion(outputs, labels)
+
+			if i%100 == 0:
+				print "Discriminator Loss : {} Generator Loss : {}".format(loss.data[0], gen_loss.data[0])
+				display(fake_inputs)
+
+			optimizer_disc.zero_grad()
+			loss.backward(retain_variables=True)
+			optimizer_disc.step()
+
+			optimizer_gen.zero_grad()
+			gen_loss.backward()			
+			optimizer_gen.step()
+
+train_gan(1)
